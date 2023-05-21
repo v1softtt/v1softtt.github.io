6e9cd6d88f74873206cc0d4a9a4f590850da74c9
Vladislav <134198572+v1softtt@users.noreply.github.com>
2023-05-21 12:39:34 +0300

    Delete v1softtt directory

--- v1softtt/js/script.js
+++
@@ -1,6 +0,0 @@
-// Появление сообщений
-let card2 = document.querySelector(".screen-one__card2", ".btn");
-card2.addEventListener("animationend", function () {
-  card2.classList.add("visible");
-});
-
