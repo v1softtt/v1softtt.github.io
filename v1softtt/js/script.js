4ad52aebea1d487c7ed1f23c4b85d2529763a7ae
Vladislav <134198572+v1softtt@users.noreply.github.com>
2023-05-21 12:32:03 +0300

    Add files via upload

---
+++ v1softtt/js/script.js
@@ -0,0 +1,6 @@
+// Появление сообщений
+let card2 = document.querySelector(".screen-one__card2", ".btn");
+card2.addEventListener("animationend", function () {
+  card2.classList.add("visible");
+});
+
